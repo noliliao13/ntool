d104ff04ff9d98a78308f39c805e9c8aaf220109
Noli <noliliao13@gmail.com>
2022-04-23 11:00:09 +0800

    📝 docs: edit basic

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -12,10 +12,11 @@ const Settings: LayoutSettings & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'NTOOL',
   pwa: false,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: '/logo.svg',
   iconfontUrl: '',
+  splitMenus: true,
 };
 
 export default Settings;
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,4 @@
 import { useIntl } from 'umi';
-import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-layout';
 
 const Footer: React.FC = () => {
@@ -14,26 +13,16 @@ const Footer: React.FC = () => {
   return (
     <DefaultFooter
       copyright={`${currentYear} ${defaultMessage}`}
-      links={[
-        {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
-          blankTarget: true,
-        },
-        {
-          key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
-          blankTarget: true,
-        },
-      ]}
+      links={
+        [
+          // {
+          //   key: 'Ant Design Pro',
+          //   title: 'Ant Design Pro',
+          //   href: 'https://pro.ant.design',
+          //   blankTarget: true,
+          // },
+        ]
+      }
     />
   );
 };
